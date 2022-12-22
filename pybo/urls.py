c4a6ea524470199c8ca0aed9c7fdcda2fa4ad908
daniel <>
2022-12-22 17:32:23 +0900

    answer comment functions added

--- pybo/urls.py
+++ pybo/urls.py
@@ -27,15 +27,28 @@ urlpatterns = [
     path('answer/delete/<int:answer_id>/',
          views.answer_delete, name='answer_delete'),
 
-    # 댓글 추가
+    # 질문 댓글 추가
     path('comment/create/question/<int:question_id>/',
          views.comment_create_question, name='comment_create_question'),
 
-    # 댓글 수정
+    # 질문 댓글 수정
     path('comment/modify/question/<int:comment_id>/',
          views.comment_modify_question, name='comment_modify_question'),
 
-    # 댓글 삭제
+    # 질문 댓글 삭제
     path('comment/delete/question/<int:comment_id>/',
          views.comment_delete_question, name='comment_delete_question'),
+
+    # 답변 댓글 추가
+    path('comment/create/answer/<int:answer_id>/',
+         views.comment_create_answer, name="comment_create_answer"),
+
+    # 답변 댓글 수정
+    path('comment/modify/answer/<int:comment_id>/',
+         views.comment_modify_answer, name="comment_modify_answer"),
+
+    # 답변 댓글 삭제
+    path('comment/delete/answer/<int:comment_id>/',
+         views.comment_delete_answer, name="comment_create_answer")
+
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -270,3 +270,72 @@ def comment_delete_question(request, comment_id):
     else:
         comment.delete()
     return redirect('pybo:detail', question_id=comment.question.id)
+
+
+@login_required(login_url='common:login')
+def comment_create_answer(request, answer_id):
+    """
+
+    Submit pybo answer's comment
+    """
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.create_date = timezone.now()
+            comment.answer = answer
+            comment.save()
+            return redirect('pybo:detail', question_id=commit.answer.question.id)
+
+    else:
+        form = CommentForm()
+    context = {'form': form}
+    return render(request, 'pybo/comment_form.html', context)
+
+
+@login_required(login_url='common:login')
+def comment_modify_answer(request, comment_id):
+    """
+
+    Edit pybo answer comment
+    """
+
+    comment = get_object_or_404(Comment, pk=comment_id)
+    if request.user != comment.author:
+        messages.error(
+            request, "you don't have permission to modify this comment")
+        return redirect('pybo:detail', question_id=comment.answer.question.id)
+
+    if request.method == 'POST':
+        form = CommentForm(request.POST, instance=comment)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.modify_date = timezone.now()
+            comment.save()
+            return redirect('pybo:detail', question_id=comment.answer.question.id)
+
+    else:
+        form = CommentForm(instance=comment)
+    context = {'form': form}
+    return render(request, 'pybo/comment_form.html', context)
+
+
+@login_required(login_url='common:login')
+def comment_delete_answer(request, comment_id):
+    """
+
+    Delete pybo answer comment
+    """
+
+    comment = get_object_or_404(Comment, pk=comment_id)
+    if request.user != comment.author:
+        messages.error(
+            request, "you don't have permission to modify this comment")
+        return redirect('pybo:detail', question_id=comment.answer.question.id)
+
+    else:
+        comment.delete()
+    return render('pybo:detail', question_id=comment.answer.question.id)
