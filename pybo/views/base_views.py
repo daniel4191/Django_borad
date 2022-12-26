98b980da24f38b39eef9298f53eefa0d5668c6e4
daniel <>
2022-12-26 19:01:26 +0900

    search, sort by conditions functions added

--- pybo/views/base_views.py
+++ pybo/views/base_views.py
@@ -1,5 +1,5 @@
 # index, detail
-
+from django.db.models import Q, Count
 from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404
 
@@ -18,15 +18,44 @@ def index(request):
     # 여기서 get인자로 들어온 page는 request에 포함된 page를 의미하는 것이고
     # 이런 page인자에 대한 넘버가 없는 경우 자동 리턴값으로 1을 추가해준것
     page = request.GET.get('page', '1')  # page
+    kw = request.GET.get('kw', '')  # 검색어
+    so = request.GET.get('so', 'recent')  # 정렬기준
+
+    # 정렬
+    if so == 'recommend':
+        question_list = Question.objects.annotate(
+            num_voter=Count('voter')).order_by('-num_voter', '-create_date')
+
+    elif so == 'popular':
+        question_list = Question.objects.annotate(
+            num_answer=Count('answer')).order_by('-num_answer', '-create_date')
+
+    else:  # recent
+        question_list = Question.objects.order_by('-create_date')
 
     # 조회
-    question_list = Question.objects.order_by('-create_date')
+    # question_list = Question.objects.order_by('-create_date')
+    if kw:
+        question_list = question_list.filter(
+            # 여기서 contains로 하게되면 대소문자를 구분해서 검색해주고
+            # icontains로 하게되면 대소문자를 가리지 않고 검색해주는 역할을 한다.
+            Q(subject__icontains=kw) |  # 제목 검색
+            Q(content__icontains=kw) |  # 내용 검색
+            Q(author__username__icontains=kw) |  # 질문 글쓴이 검색
+            Q(answer__author__username__icontains=kw)  # 답변 글쓴이 검색
+            # distinct는 조회결과의 중복을 제거한후에 반환 한다
+        ).distinct()
 
     # 페이징 처리
     paginator = Paginator(question_list, 10)  # 페이지당 10개의 포스트
     page_obj = paginator.get_page(page)
 
-    context = {'question_list': page_obj}
+    context = {
+        'question_list': page_obj,
+        'page': page,
+        'kw': kw,
+        'so': so
+    }
     return render(request, 'pybo/question_list.html', context)
 
     # 코드 1
